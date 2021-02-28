c1d7fdb34530d90daa7f563dcf6668f4f447e745
mkpMac <mkp0131@naver.com>
2021-02-28 13:06:54 +0900

    3

--- src/index.js
+++ src/index.js
@@ -1,21 +1,50 @@
+import { createStore } from "redux";
+
 const formTag = document.querySelector(".js-form");
 const inputTag = document.querySelector(".js-input");
 const listTag = document.querySelector(".js-list");
-const todoList = [];
 
-const updateList = () => {
-  const items = todoList.map((item) => {
-    return `<li>${item}</li>`;
+const todoList = (state = [], action) => {
+  switch (action.type) {
+    case "add":
+      return [{ todo: action.text, id: Date.now() }, ...state];
+    case "delete":
+      return state.filter((item) => item.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+let store = createStore(todoList);
+
+store.subscribe(() => {
+  updateList(store.getState());
+});
+
+const deleteTodo = (id) => {
+  store.dispatch({ type: "delete", id: id });
+};
+
+const updateList = (todoList) => {
+  listTag.innerHTML = "";
+  todoList.forEach((item) => {
+    const li = document.createElement("li");
+    const btnDelete = document.createElement("button");
+    btnDelete.innerText = "삭제";
+    btnDelete.addEventListener("click", () => {
+      deleteTodo(item.id);
+    });
+    li.innerText = item.todo;
+    li.appendChild(btnDelete);
+    listTag.appendChild(li);
   });
-  console.log("items", items);
-  listTag.innerHTML = items.join("");
 };
 
 const addList = (e) => {
   e.preventDefault();
   const value = inputTag.value;
-  todoList.push(value);
-  updateList();
+  if (!value.length) return;
+  store.dispatch({ type: "add", text: value });
   inputTag.value = "";
 };
 
